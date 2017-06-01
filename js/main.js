998653a0cfeb06a4454d702ad29f828c8e1a4722
Liam McCann <liam.mccann@geeks.ltd.uk>
2017-06-01 15:26:53 +0100

    Second pull request

--- js/main.js
+++ js/main.js
@@ -1,31 +1,80 @@
-var imageLoader = document.getElementById('imageLoader');
-    imageLoader.addEventListener('change', handleImage, false);
+var imageLoader = $('#imageLoader');
+    imageLoader.change(handleImage);
+
 var iconLinks = $(".icon-row img");
     iconLinks.click(addToCanvas);
-var canvas = document.getElementById('imageCanvas');
-var ctx = canvas.getContext('2d');
 
-function handleImage(e){
+var actions = $(".actions");
+
+var canvas = new fabric.Canvas('imageCanvas', { width: 1200, height: 400 }); // Calculate these from viewport size? Or parent element?
+
+fabric.Canvas.prototype.customiseControls({
+    tr: {
+        action: 'remove',
+        cursor: 'pointer'
+    },
+    bl: {
+        action: 'scale'
+    },
+    br: {
+        action: 'rotate',
+        cursor: 'pointer'
+    } 
+}, function() {
+    canvas.renderAll();
+});
+
+var HideControls = {
+    tl: false,
+    mtr: false,
+    mt: false,
+    mr: false,
+    mb: false,
+    ml: false,
+};
+
+function handleImage(e) {
     var reader = new FileReader();
-    reader.onload = function(event){
-        var img = new Image();
-        img.onload = function(){
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img,0,0);
-            iconLinks.show();
-        }
-        img.src = event.target.result;
-    }
     reader.readAsDataURL(e.target.files[0]);     
+    reader.onload = function(event) {
+        var img = new Image();
+        img.src = event.target.result;
+        img.onload = function() {
+            canvas.setWidth(img.width);
+            canvas.setHeight(img.height);
+            canvas.setBackgroundImage(event.target.result, canvas.renderAll.bind(canvas));
+
+            actions.show();
+            iconLinks.show();
+            imageLoader.closest('form').hide();
+        }
+    }
 }
 
-function addToCanvas(){
+function addToCanvas() {
     var imageUrl = $(this).attr("src");
     var img = new Image();
     img.src = imageUrl;
 
-    img.onload = function(){
-        ctx.drawImage(img,0,0);
+    img.onload = function() {
+        var imgInstance = new fabric.Image(img, {
+          left: 15,
+          top: 15
+        });
+
+        if ((img.width + 30) > canvas.width)
+            imgInstance.scaleToWidth(canvas.width - 30);
+
+        var reducedBy = canvas.width / img.width;
+
+        if ((reducedBy * img.height) > canvas.height)
+            imgInstance.scaleToHeight(canvas.height - 30);
+
+        imgInstance.setControlsVisibility(HideControls);
+        canvas.add(imgInstance);
     }
 }
+
+$("#action--download").click(function() {
+    window.open(canvas.toDataURL());
+});
